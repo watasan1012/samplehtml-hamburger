f4325f7a4a52e04cfb7de61667f6b30f798de3f5
watasan <ishiwatahirotaka1012@gmail.com>
2023-02-08 18:48:31 +0900

    humburger add

---
+++ assets/js/hamburger.js
@@ -0,0 +1,6 @@
+
+$('.burger-btn').on('click',function(){//.btn_triggerをクリックすると
+    $('.burger-btn').toggleClass('close');//.btn_triggerにcloseクラスを付与(ボタンのアニメーション)
+    $('.nav-wrapper').toggleClass('fade');//.nav-wrapperに
+    $('body').toggleClass('noscroll');//bodyにnoscrollクラスを付与(スクロールを固定)
+   });
